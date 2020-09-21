af92d59ebfcb93cef49bd8c2789d0d01926fd575
Diogo Soares <diogo.soares@dbccompany.com.br>
2020-09-21 10:54:19 -0300

    Initial commit.

---
+++ Api/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
